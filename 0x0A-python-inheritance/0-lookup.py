15ece00f73e653c403f37277d79a88a180e7670b
Obadia-Gilbert <68857383+Obadia-Gilbert@users.noreply.github.com>
2023-01-09 19:16:08 +0300

    Create 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-lookup.py
+# Obadia Gilbert <gilbertobadia830@gmail.com>
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
