a2c5d59bff4a91c75cc2f9dde676a7e74e6eb926
Obadia-Gilbert <68857383+Obadia-Gilbert@users.noreply.github.com>
2023-01-11 18:44:46 +0300

    Update 14-pascal_triangle.py

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 14-pascal_triangle.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Obadia Gilbert <gilbertobadia830@gmail.com>
 """Defines a Pascal's Triangle function."""
 
 
